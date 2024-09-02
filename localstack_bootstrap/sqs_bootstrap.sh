f8476e22ef30b9a7f2c95de06db7db8b999ccd17
Júlio Nepomuceno <julioreplay@gmail.com>
2024-09-02 11:32:11 -0300

    feat: create rampup aws

---
+++ localstack_bootstrap/sqs_bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# enable debug
+# set -x
+
+echo "configuring sqs"
+echo "==================="
+LOCALSTACK_HOST=localhost
+AWS_REGION=eu-central-1
+
+create_queue() {
+    local QUEUE_NAME_TO_CREATE=$1
+    awslocal sqs create-queue --queue-name ${QUEUE_NAME_TO_CREATE}
+}
+
+create_queue "queue1"
+create_queue "queue2"
+
+echo "finished"
